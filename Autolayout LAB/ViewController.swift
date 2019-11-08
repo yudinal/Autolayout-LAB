0a6801749c3af5f6213587d0b9c351ae55a27fae
Lilia Yudina <liliayudina@gmail.com>
2019-11-08 10:55:27 -0500

    Initial Commit

---
+++ Autolayout LAB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Autolayout LAB
+//
+//  Created by Lilia Yudina on 11/7/19.
+//  Copyright © 2019 Lilia Yudina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
